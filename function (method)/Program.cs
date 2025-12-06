6d7e74cf0c91c354cd235a31d9161c576727bd6d
mohamednasser2001 <mmm.zzz155mody@gmail.com>
2025-12-07 00:01:13 +0200

    update new C#

--- function (method)/Program.cs
+++ function (method)/Program.cs
@@ -6,10 +6,18 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
+        static void mainas(int num1,int num2)
+        {
+            Console.WriteLine($"num1-num2= {num1 - num2}");
+        }
         static void Main(string[] args)
         {
            // Console.WriteLine("Hello, World!");
            hello();
+            int num1 = 10;
+            int num2 = 20;
+            mainas(num1: num1,num2:num2);
         }
     }
 }
